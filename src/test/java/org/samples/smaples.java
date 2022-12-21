18ab8447835920114774f8c23cf66eaf92e520ae
Deepikaselladurai <deepikadeepi880@gmail.com>
2022-12-21 21:51:05 +0530

    Kumar code

---
+++ src/test/java/org/samples/smaples.java
@@ -0,0 +1,10 @@
+package org.samples;
+
+public class smaples {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Earth");
+		System.out.println("What are doing");
+	}
+
+}
